5e129e5c1a2c0cbcb6e34756de7e352d70166ad3
unknown <saidgm.code.edu.az>
2021-12-13 19:17:39 +0400

    Homework

---
+++ Operator overloading/OperatorOverloading/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surame { get; set; }
+        public int Age { get; set; }
+
+        Person[] arr;
+
+        public Person()
+        {
+            arr = new Person[0];
+        }
+        public void Add(Person obj)
+        {
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = obj;
+        }
+        public void GetAll()
+        {
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item.Age);
+            }
+        }
+    }
+}
---
+++ Operator overloading/OperatorOverloading/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OperatorOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person list = new Person();
+            #region PersonArr
+            list.Add(new Person { Age = 17 });
+            list.Add(new Person { Age = 18 });
+            list.Add(new Person { Age = 19 });
+            list.Add(new Person { Age = 20 });
+            //list.GetAll();
+            #endregion
+        }
+    }
+}
